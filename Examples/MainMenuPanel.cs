f9815e8500204a5780b67053b028f991ffa3a180
Umair <umairsaifullah01@gmail.com>
2024-12-30 01:00:01 +0500

    update structure

--- Examples/MainMenuPanel.cs
+++ Examples/MainMenuPanel.cs
@@ -33,10 +33,10 @@ namespace THEBADDEST.UI
 
 		void Animation()
 		{
-			Tweener tweener = new Tweener(views["CenterBar"].GetTransform(), StartCoroutine);
-			tweener.MoveLocal(Vector3.up * 1250, Vector3.up * 710f, 1f).SetEase(TweenerEasing.Ease.EaseInElastic);
-			tweener = new Tweener(views["Play"].GetTransform(), StartCoroutine);
-			tweener.Scale(Vector3.zero, Vector3.one, 1f).SetEase(TweenerEasing.Ease.EaseInBounce);
+			// Tweener tweener = new Tweener(views["CenterBar"].GetTransform(), StartCoroutine);
+			// tweener.MoveLocal(Vector3.up * 1250, Vector3.up * 710f, 1f).SetEase(TweenerEasing.Ease.EaseInElastic);
+			// tweener = new Tweener(views["Play"].GetTransform(), StartCoroutine);
+			// tweener.Scale(Vector3.zero, Vector3.one, 1f).SetEase(TweenerEasing.Ease.EaseInBounce);
 		}
 
 	}
