6f44a5e55b1cf7ad0f1a09dfa8d2825a79e2ad8e
Umair <umairsaifullah01@gmail.com>
2025-01-06 19:14:42 +0500

    Button View with Tween Added

--- Runtime/Components/ButtonView.cs
+++ Runtime/Components/ButtonView.cs
@@ -1,31 +1,49 @@
 using System;
 using THEBADDEST.MVVM;
-using THEBADDEST.UI;
+using UnityEngine;
 using UnityEngine.EventSystems;
-
-public class ButtonView : ViewBase, IPointerClickHandler
+using THEBADDEST.Tweening;
+namespace THEBADDEST.UI
 {
-
-	Action onclickEvent;
-
-	public override void Init(IViewModel viewModel)
+	public class ButtonView : ViewBase, IPointerClickHandler
 	{
-		base.Init(viewModel);
-		this.ViewModel.ModelBinder += Bind;
-	}
 
-	void Bind(string id, IModel<object> model)
-	{
-		if (!this.Id.Equals(id)) return;
-		if (model.Data is Action action)
+		[SerializeField] protected Tween clickTween;
+		protected Action onclickEvent;
+
+		public override void Init(IViewModel viewModel)
 		{
-			onclickEvent = action;
+			base.Init(viewModel);
+			ViewModel.ModelBinder += EventBind;
+			clickTween.onComplete += () => onclickEvent?.Invoke();
 		}
-	}
 
-	public void OnPointerClick(PointerEventData eventData)
-	{
-		onclickEvent?.Invoke();
-	}
+		void EventBind(string id, IModel<object> model)
+		{
+			if (!this.Id.Equals(id)) return;
+			if (model.Data is Action action)
+			{
+				onclickEvent = action;
+			}
+		}
 
+
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			PerformClick();
+		}
+
+		protected virtual void PerformClick()
+		{
+			if (clickTween != null)
+			{
+				StartCoroutine(clickTween.Play(transform));
+			}
+			else
+			{
+				onclickEvent?.Invoke();
+			}
+		}
+
+	}
 }
