f65fc9026aee15a08c215bfcdbab3359417f9f2d
Umair <umairsaifullah01@gmail.com>
2025-01-09 19:39:00 +0500

    toggle View Updated

--- Runtime/Components/ToggleView.cs
+++ Runtime/Components/ToggleView.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
+﻿using THEBADDEST.MVVM;
 using UnityEngine;
+using System;
+using System.Collections;
+using THEBADDEST.Tweening;
+using UnityEngine.EventSystems;
 
 
 namespace THEBADDEST.UI
 {
 
 
-	public class ToggleView : ButtonView
+	public class ToggleView : ViewBase, IPointerClickHandler
 	{
+		[SerializeField] protected Tween     clickTween;
+		[SerializeField]           Transform onObject;
+		[SerializeField]           Transform offObject;
+		
+		Action<bool> onValueChanged;
+		
 
-		[SerializeField] Transform onObject;
-		[SerializeField] Transform offObject;
+		public override void Init(IViewModel viewModel)
+		{
+			base.Init(viewModel);
+			viewModel.ModelBinder+= Bind;
+		}
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			PerformClick();
+		}
+		void Bind(string id, IModel<object> model)
+		{
+			if (!this.Id.Equals(id)) return;
+			if (model.Data is Action<bool> action)
+			{
+				onValueChanged = action;
+			}
 
-		protected override void PerformClick()
+			if (model.Data is float value)
+			{
+				var isOn= Mathf.Approximately(value, 1);
+				onObject.gameObject.SetActive(isOn);
+				offObject.gameObject.SetActive(!isOn);
+			}
+		}
+
+		void PerformClick()
 		{
 			if (onObject.gameObject.activeSelf)
 			{
@@ -27,9 +59,16 @@ namespace THEBADDEST.UI
 				StartCoroutine(PlayTween(onObject));
 			}
 			if(!clickTween)
-				onclickEvent?.Invoke();
+			{
+				onValueChanged?.Invoke(onObject.gameObject.activeSelf);     
+			}
 		}
-		
+
+		IEnumerator PlayTween(Transform target)
+		{
+			yield return clickTween.Play(target);
+			onValueChanged?.Invoke(onObject.gameObject.activeSelf);     
+		}
 
 	}
 }
