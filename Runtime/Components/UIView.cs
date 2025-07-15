6c2b60dd549a00c1bd5a5e8551252dbf81e6a893
Umair <umairsaifullah01@gmail.com>
2025-07-15 16:00:16 +0500

    uivew added

---
+++ Runtime/Components/UIView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+
+namespace THEBADDEST.UI
+{
+
+
+	public class UIView : ViewBase
+	{
+
+		[SerializeField, HideInInspector] protected RectTransform cachedRectTransform;
+		/// <summary>
+		/// Retrieves the transform component of the view.
+		/// </summary>
+		public RectTransform rectTransformObject
+		{
+			get
+			{
+				if (cachedRectTransform == null) cachedRectTransform = GetComponent<RectTransform>();
+				return cachedRectTransform;
+			}
+		}
+
+	}
+
+
+}
